f1e0d786268a201a42c4d8034356c85323520c8a
Robin Arys <robin@arys.dev>
2021-10-02 12:31:00 +0200

    Add a heat build-up effect and an explosion effect

--- Game.gd
+++ Game.gd
@@ -3,18 +3,26 @@ extends Spatial
 onready var gauge := find_node("HeatGauge") as HeatGauge
 onready var pot := find_node("Pot") as Pot
 onready var progress_circle := find_node("ProgressCircle") as ProgressCircle
+onready var anim_player := find_node("AnimationPlayer") as AnimationPlayer
 
 var time_in_range := 0.0
 var time_in_danger := 0.0
 var in_range := false
 var in_danger := false
 
-var potion_duration := 1.0
+var potion_duration := 4.0
+var time_bubbles := 2.0
+var buildup_speed := 0.5
+var buildup_explosion := 2.0
+
+func _ready():
+	pot.emit_fire = true
+	pot.emit_bubbles = false
 
 func _process(delta):
 	if self.in_range:
 		time_in_range += delta
-		if time_in_range > 2:
+		if time_in_range >= time_bubbles:
 			pot.emit_bubbles = true
 
 	progress_circle.progress = time_in_range / potion_duration
@@ -32,16 +40,21 @@ func _process(delta):
 	else:
 		gauge.range_color = Color.white
 
+	if self.in_danger:
+		pot.set_buildup_strength(time_in_danger * buildup_speed)
+		if pot.buildup_strength > buildup_explosion:
+			self.anim_player.play("gameover")
+	elif pot.buildup_strength > 0.0:
+		pot.set_buildup_strength(lerp(pot.buildup_strength, 0, delta))
+
 
 func _on_HeatGauge_stir_in_range():
 	self.in_range = true
-	pot.emit_fire = true
 
 
 func _on_HeatGauge_stir_out_of_range():
 	self.in_range = false
 	time_in_range = 0
-	pot.emit_fire = false
 	pot.emit_bubbles = false
 
 
--- Pot/Pot.gd
+++ Pot/Pot.gd
@@ -2,20 +2,27 @@ extends StaticBody
 class_name Pot
 
 var is_hovered := false
+var buildup_strength: float setget set_buildup_strength
 
 export(Color) var bubble_color := Color(0.3, 0.5, 0.75, 0.35) setget set_bubble_color
 export(Color) var liquid_color := Color(0.3, 0.5, 0.75, 0.5) setget set_liquid_color
+export(Color) var overheat_color := Color(0.54, 0, 0)
 
 export(bool) var emit_bubbles := false setget set_bubbles
 export(bool) var emit_fire := false setget set_fire
 export(bool) var liquid_light := false setget set_liquid_light
 
+onready var msh_pot := find_node("Pot") as MeshInstance
+onready var msh_border := find_node("Border") as MeshInstance
 onready var anim_spoon := find_node("AnimSpoon") as AnimationPlayer
 onready var anim_pot := find_node("AnimPot") as AnimationPlayer
 onready var pot_light := find_node("PotLight") as OmniLight
 onready var liquid := find_node("Liquid") as CSGMesh
 onready var liquid_particles := find_node("LiquidParticles") as CPUParticles
 onready var fire_particles := find_node("FireParticles") as CPUParticles
+
+var _pot_default: Color
+var _border_default: Color
 
 signal stirred
 
@@ -26,6 +33,8 @@ func _ready() -> void:
 	self.connect("mouse_entered", self, "set_hovered", [true])
 	self.connect("mouse_exited", self, "set_hovered", [false])
 	self.set_bubbles(self.emit_bubbles)
+	self._pot_default = msh_pot.get_surface_material(0).albedo_color
+	self._border_default = msh_border.get_surface_material(0).albedo_color
 
 
 func set_hovered(hovered: bool) -> void:
@@ -37,6 +46,19 @@ func _unhandled_input(event) -> void:
 		anim_spoon.play("Stir")
 		anim_pot.play("hiteffect")
 		self.emit_signal("stirred")
+
+
+func set_buildup_strength(strength: float) -> void:
+	buildup_strength = strength
+	anim_pot.play("buildup", -1, buildup_strength)
+	var pot_color = self._pot_default.linear_interpolate(overheat_color, buildup_strength)
+	msh_pot.get_surface_material(0).albedo_color = pot_color
+	var border_color = self._border_default.linear_interpolate(overheat_color, buildup_strength)
+	msh_border.get_surface_material(0).albedo_color = border_color
+
+
+func stop_buildup() -> void:
+	anim_pot.stop()
 
 
 func set_bubbles(bubbles: bool) -> void:
