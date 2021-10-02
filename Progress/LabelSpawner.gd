a0cd7ee1781566be206eaf74e35b9d3223870fb3
Robin Arys <robin@arys.dev>
2021-10-02 20:05:15 +0200

    Remove unnecessary label offset

--- Progress/LabelSpawner.gd
+++ Progress/LabelSpawner.gd
@@ -4,7 +4,4 @@ class_name LabelSpawner
 func spawn_label(text: String) -> void:
 	var label := preload("res://Progress/FloatingLabel.tscn").instance() as Label
 	label.text = text
-	var x := rand_range(-2.0, 2.0)
-	var y := rand_range(-2.0, 2.0)
 	self.add_child(label)
-	label.rect_global_position += Vector2(x, y)
