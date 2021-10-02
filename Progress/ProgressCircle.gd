9cefe1a6ff1ce56901bffd4464f045f07828c417
Robin Arys <robin@arys.dev>
2021-10-02 18:34:08 +0200

    Reset the progress circle

--- Progress/ProgressCircle.gd
+++ Progress/ProgressCircle.gd
@@ -7,6 +7,10 @@ export(Color) var foreground_color := Color(0.14, 0.36, 0.25) setget set_fg
 export(float, 0.0, 1.0) var progress := 0.0 setget set_progress
 
 
+func reset() -> void:
+	self.progress = 0.0
+
+
 func set_bg(color: Color) -> void:
 	background_color = color
 	self.material.set_shader_param("background_color", background_color)
