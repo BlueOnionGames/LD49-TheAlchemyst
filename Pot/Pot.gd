621a06bdf58ef36d3772ef65d9fb49440a740f62
Robin Arys <robin@arys.dev>
2021-10-04 19:14:46 +0200

    Fix a bug with touch input spam-stirring sometimes

--- Pot/Pot.gd
+++ Pot/Pot.gd
@@ -52,7 +52,7 @@ func _ready() -> void:
 
 
 func _unhandled_input(event) -> void:
-	if (event is InputEventScreenTouch or event.is_action_pressed("game_stir")) and self.is_hovered:
+	if ((event is InputEventScreenTouch and event.pressed) or event.is_action_pressed("game_stir")) and self.is_hovered:
 		Stats.manual_stirs += 1
 		self.stir()
 		get_tree().set_input_as_handled()
